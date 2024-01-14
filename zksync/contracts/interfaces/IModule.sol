6dcd3489d0cc34a6beda91db43dd183cc0ff122f
tahos81 <tahirozpala@gmail.com>
2024-01-14 17:20:02 +0300

    seperate contract repos

--- zksync/contracts/interfaces/IModule.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.17;
-
-import {IInitable} from '../interfaces/IInitable.sol';
-import {IERC165} from '@openzeppelin/contracts/utils/introspection/IERC165.sol';
-
-interface IModule is IInitable, IERC165 {}
