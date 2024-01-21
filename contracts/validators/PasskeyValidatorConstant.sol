232b71b79c39fe575fdb0752f147bb7413ec63c5
tahos81 <tahirozpala@gmail.com>
2024-01-21 23:55:20 +0300

    feat: allow taking auth data from outside

--- contracts/validators/PasskeyValidatorConstant.sol
+++ contracts/validators/PasskeyValidatorConstant.sol
@@ -8,7 +8,9 @@ import {Errors} from '../libraries/Errors.sol';
 contract PasskeyValidatorConstant is IR1Validator {
     address constant P256_VERIFIER = 0x4323cffC1Fda2da9928cB5A5A9dA45DC8Ee38a2f;
     string constant ClIENT_DATA_PREFIX = '{"type":"webauthn.get","challenge":"';
-    string constant ClIENT_DATA_SUFFIX = '","origin":"https://getclave.io"}';
+    string constant IOS_ClIENT_DATA_SUFFIX = '","origin":"https://getclave.io"}';
+    string constant ANDROID_ClIENT_DATA_SUFFIX =
+        '","origin":"android:apk-key-hash:-sYXRdwJA3hvue3mKpYrOZ9zSPC7b4mbgzJmdZEDO5w","androidPackageName":"com.clave.mobile"}';
     bytes constant AUTHENTICATOR_DATA =
         hex'175faf8504c2cdd7c01778a8b0efd4874ecb3aefd7ebb7079a941f7be8897d411d00000000';
 
@@ -17,20 +19,10 @@ contract PasskeyValidatorConstant is IR1Validator {
         bytes calldata signature,
         bytes32[2] calldata pubKey
     ) external view returns (bool valid) {
-        bytes memory challengeBase64 = bytes(Base64Url.encode(bytes.concat(challenge)));
-        bytes memory clientData = bytes.concat(
-            bytes(ClIENT_DATA_PREFIX),
-            challengeBase64,
-            bytes(ClIENT_DATA_SUFFIX)
-        );
-        bytes32[2] memory rs = abi.decode(signature, (bytes32[2]));
-
-        bytes32 message = createMessage(AUTHENTICATOR_DATA, clientData);
-
-        bool isValidSig = callVerifier(message, rs, pubKey);
-
-        if (isValidSig) {
-            valid = true;
+        if (signature.length == 65) {
+            valid = _validateSignature(challenge, signature, pubKey);
+        } else {
+            valid = _validateFatSignature(challenge, signature, pubKey);
         }
     }
 
@@ -76,61 +68,79 @@ contract PasskeyValidatorConstant is IR1Validator {
         return false;
     }
 
-    /**
-     * @notice Compute the webauthn message from the authenticator data and client data
-     * @param authenticatorData bytes - Authenticator data obtained by the fat signature
-     * @param clientData bytes        - Client data obtained by the fat signature
-     * @return message bytes32 - Webauthn message to be used for signature validation
-     */
-    function createMessage(
+    function _validateSignature(
+        bytes32 challenge,
+        bytes calldata signature,
+        bytes32[2] calldata pubKey
+    ) private view returns (bool valid) {
+        bool isAndroid = signature[0] == 0x00;
+        bytes32[2] memory rs = abi.decode(signature[1:], (bytes32[2]));
+        bytes memory challengeBase64 = bytes(Base64Url.encode(bytes.concat(challenge)));
+        bytes memory clientData;
+        if (isAndroid) {
+            clientData = bytes.concat(
+                bytes(ClIENT_DATA_PREFIX),
+                challengeBase64,
+                bytes(ANDROID_ClIENT_DATA_SUFFIX)
+            );
+        } else {
+            clientData = bytes.concat(
+                bytes(ClIENT_DATA_PREFIX),
+                challengeBase64,
+                bytes(IOS_ClIENT_DATA_SUFFIX)
+            );
+        }
+
+        bytes32 message = _createMessage(AUTHENTICATOR_DATA, clientData);
+
+        valid = callVerifier(message, rs, pubKey);
+    }
+
+    function _validateFatSignature(
+        bytes32 challenge,
+        bytes calldata fatSignature,
+        bytes32[2] calldata pubKey
+    ) private view returns (bool valid) {
+        (
+            bytes memory authenticatorData,
+            string memory clientDataSuffix,
+            bytes32[2] memory rs
+        ) = _decodeFatSignature(fatSignature);
+
+        bytes memory challengeBase64 = bytes(Base64Url.encode(bytes.concat(challenge)));
+        bytes memory clientData = bytes.concat(
+            bytes(ClIENT_DATA_PREFIX),
+            challengeBase64,
+            bytes(clientDataSuffix)
+        );
+
+        bytes32 message = _createMessage(authenticatorData, clientData);
+
+        valid = callVerifier(message, rs, pubKey);
+    }
+
+    function _createMessage(
         bytes memory authenticatorData,
         bytes memory clientData
     ) private pure returns (bytes32 message) {
-        bytes memory verifyData = new bytes(authenticatorData.length + 32);
-
-        copyBytes(authenticatorData, 0, authenticatorData.length, verifyData, 0);
-
-        copyBytes(
-            abi.encodePacked(sha256(clientData)),
-            0,
-            32,
-            verifyData,
-            authenticatorData.length
-        );
-
-        message = sha256(verifyData);
+        bytes32 clientDataHash = sha256(clientData);
+        message = sha256(bytes.concat(authenticatorData, clientDataHash));
     }
 
-    /**
-     * @notice Helper function for copying from one place in memory to another
-     * @param _from bytes         - Source byte array to copy from
-     * @param _fromOffset uint256 - Starting point in the source array to begin copying from
-     * @param _length uint256     - Number of bytes to be copied
-     * @param _to bytes           - Destination byte array to copy to
-     * @param _toOffset uint256   - Position in the destination array where copied bytes will be placed
-     * @return _to bytes          - Returns the destination byte array
-     */
-
-    function copyBytes(
-        bytes memory _from,
-        uint256 _fromOffset,
-        uint256 _length,
-        bytes memory _to,
-        uint256 _toOffset
-    ) private pure returns (bytes memory) {
-        uint256 minLength = _length + _toOffset;
-        require(_to.length >= minLength, '[copyBytes] Buffer too small.');
-        // The offset 32 is added to skip the size field of both bytes variables
-        uint256 i = 32 + _fromOffset;
-        uint256 j = 32 + _toOffset;
-        while (i < (32 + _fromOffset + _length)) {
-            assembly {
-                let tmp := mload(add(_from, i))
-                mstore(add(_to, j), tmp)
-            }
-            i += 32;
-            j += 32;
-        }
-        return _to;
+    function _decodeFatSignature(
+        bytes memory fatSignature
+    )
+        private
+        pure
+        returns (
+            bytes memory authenticatorData,
+            string memory clientDataSuffix,
+            bytes32[2] memory rs
+        )
+    {
+        (authenticatorData, clientDataSuffix, rs) = abi.decode(
+            fatSignature,
+            (bytes, string, bytes32[2])
+        );
     }
 }
