066a77e4da1cc0340d19e0b51f16e7bb3d226f5d
tahos81 <tahirozpala@gmail.com>
2024-01-29 14:47:11 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,23 +12,36 @@ import type { NetworkUserConfig } from 'hardhat/types';
 
 dotenv.config();
 
-// dynamically changes endpoints for local tests
-const zkSyncTestnet: NetworkUserConfig =
-    process.env.NODE_ENV == 'test' || process.env.NODE_ENV == 'snapshot'
-        ? {
-              url: 'http://127.0.0.1:8011',
-              ethNetwork: 'http://127.0.0.1:8545',
-              zksync: true,
-          }
-        : {
-              url: 'https://mainnet.era.zksync.io',
-              ethNetwork: 'mainnet',
-              zksync: true,
-              // contract verification endpoint
-              verifyURL:
-                  'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
-              chainId: 324,
-          };
+const zkSyncMainnet: NetworkUserConfig = {
+    url: 'https://mainnet.era.zksync.io',
+    ethNetwork: 'mainnet',
+    zksync: true,
+    verifyURL:
+        'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+    chainId: 324,
+};
+
+const zkSyncSepolia: NetworkUserConfig = {
+    url: 'https://sepolia.era.zksync.dev',
+    ethNetwork: 'sepolia',
+    zksync: true,
+    verifyURL: 'https://explorer.sepolia.era.zksync.dev/contract_verification',
+    chainId: 300,
+};
+
+const inMemoryNode: NetworkUserConfig = {
+    url: 'http://127.0.0.1:8011',
+    ethNetwork: '', // in-memory node doesn't support eth node; removing this line will cause an error
+    zksync: true,
+    chainId: 260,
+};
+
+const dockerizedNode: NetworkUserConfig = {
+    url: 'http://localhost:3050',
+    ethNetwork: 'http://localhost:8545',
+    zksync: true,
+    chainId: 270,
+};
 
 const config: HardhatUserConfig = {
     zksolc: {
@@ -40,15 +53,18 @@ const config: HardhatUserConfig = {
             },
         },
     },
-    defaultNetwork: 'zkSyncTestnet',
+    defaultNetwork: 'zkSyncSepolia',
     networks: {
         hardhat: {
-            zksync: false,
+            zksync: true,
         },
-        zkSyncTestnet,
+        zkSyncSepolia,
+        zkSyncMainnet,
+        inMemoryNode,
+        dockerizedNode,
     },
     solidity: {
-        compilers: [{ version: '0.8.17' }],
+        version: '0.8.17',
     },
 };
 
