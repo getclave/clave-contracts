4bd66b6def849a82ed0e44dba753e71f777929ce
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2024-02-24 11:41:14 -0700

    [clave-contracts] fix: remove erc777 interface

--- contracts/helpers/TokenCallbackHandler.sol
+++ contracts/helpers/TokenCallbackHandler.sol
@@ -1,18 +1,17 @@
-// Copied from '@account-abstraction/contracts/samples/callback/TokenCallbackHandler.sol'
-
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.12;
 
-import {IERC165} from '@openzeppelin/contracts/utils/introspection/IERC165.sol';
-import {IERC777Recipient} from '@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol';
-import {IERC721Receiver} from '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
-import {IERC1155Receiver} from '@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol';
+/* solhint-disable no-empty-blocks */
+
+import '@openzeppelin/contracts/utils/introspection/IERC165.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol';
 
 /**
  * Token callback handler.
  *   Handles supported tokens' callbacks, allowing account receiving these tokens.
  */
-contract TokenCallbackHandler is IERC777Recipient, IERC721Receiver, IERC1155Receiver {
+contract TokenCallbackHandler is IERC721Receiver, IERC1155Receiver {
     function tokensReceived(
         address,
         address,
