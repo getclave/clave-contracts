46e54a39c9ab7040aa20f4a97b395ef8b424a038
tahos81 <tahirozpala@gmail.com>
2024-03-22 16:02:04 +0300

    mapper function for erc20 Transfer event

---
+++ subgraph/src/erc20.ts
@@ -0,0 +1,47 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { Transfer as TransferEvent } from '../generated/erc20/ERC20';
+import { ClaveAccount, Token } from '../generated/schema';
+import {
+    decreaseAccountBalance,
+    fetchTokenDecimals,
+    fetchTokenName,
+    fetchTokenSymbol,
+    increaseAccountBalance,
+    toDecimal,
+} from './helpers';
+
+export function handleTransfer(event: TransferEvent): void {
+    const from = event.params.from;
+    const to = event.params.to;
+
+    const fromAccount = ClaveAccount.load(from);
+    const toAccount = ClaveAccount.load(to);
+
+    //not a Clave related transfer
+    if (fromAccount === null || toAccount === null) {
+        return;
+    }
+
+    const tokenAddress = event.address;
+    let token = Token.load(tokenAddress);
+    if (!token) {
+        token = new Token(tokenAddress);
+        token.name = fetchTokenName(tokenAddress);
+        token.symbol = fetchTokenSymbol(tokenAddress);
+        token.decimals = fetchTokenDecimals(tokenAddress) as i32;
+    }
+
+    const amount = toDecimal(event.params.value, token.decimals);
+
+    const tokenBalanceFrom = decreaseAccountBalance(fromAccount, token, amount);
+    const tokenBalanceTo = increaseAccountBalance(toAccount, token, amount);
+
+    token.save();
+    tokenBalanceFrom.save();
+    tokenBalanceTo.save();
+}
