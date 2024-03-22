a1601e5e0eb4d02bb8fa57ef7ffd582557c78b56
tahos81 <tahirozpala@gmail.com>
2024-03-22 16:00:55 +0300

    mapper function for NewClaveAccount event

--- subgraph/src/account-factory.ts
+++ subgraph/src/account-factory.ts
@@ -3,18 +3,14 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-import type { NewClaveAccount as NewClaveAccountEvent } from '../generated/AccountFactory/AccountFactory';
-import { NewClaveAccount } from '../generated/schema';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { NewClaveAccount as NewClaveAccountEvent } from '../generated/AccountFactory/AccountFactory';
+import { ClaveAccount } from '../generated/schema';
 
 export function handleNewClaveAccount(event: NewClaveAccountEvent): void {
-    const entity = new NewClaveAccount(
-        event.transaction.hash.concatI32(event.logIndex.toI32()),
-    );
-    entity.accountAddress = event.params.accountAddress;
+    const account = new ClaveAccount(event.params.accountAddress);
 
-    entity.blockNumber = event.block.number;
-    entity.blockTimestamp = event.block.timestamp;
-    entity.transactionHash = event.transaction.hash;
+    account.deployDate = event.block.timestamp;
 
-    entity.save();
+    account.save();
 }
