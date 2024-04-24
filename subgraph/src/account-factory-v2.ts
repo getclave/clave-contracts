6afed2e6ea758de96523f239df116a682117dfae
tahos81 <tahirozpala@gmail.com>
2024-04-24 16:41:51 +0300

    Update account-factory-v2.ts

--- subgraph/src/account-factory-v2.ts
+++ subgraph/src/account-factory-v2.ts
@@ -12,20 +12,21 @@ import {
     ClaveAccountDeployed as ClaveAccountDeployedEvent,
 } from '../generated/Contract/Contract';
 import { ClaveAccount } from '../generated/schema';
+import { ZERO, getOrCreateWeek } from './helpers';
 
 export function handleClaveAccountCreated(
     event: ClaveAccountCreatedEvent,
 ): void {
     const account = new ClaveAccount(event.params.accountAddress);
+    const week = getOrCreateWeek(event.block.timestamp);
 
+    week.createdAccounts = week.createdAccounts + 1;
     account.creationDate = event.block.timestamp;
-    account.implementation = Bytes.fromHexString(
-        '0xf5bEDd0304ee359844541262aC349a6016A50bc6',
-    );
     account.hasRecovery = false;
     account.isRecovering = false;
     account.recoveryCount = 0;
 
+    week.save();
     account.save();
 }
 
@@ -33,18 +34,22 @@ export function handleClaveAccountDeployed(
     event: ClaveAccountDeployedEvent,
 ): void {
     let account = ClaveAccount.load(event.params.accountAddress);
+    const week = getOrCreateWeek(event.block.timestamp);
+    week.deployedAccounts = week.deployedAccounts + 1;
     if (!account) {
         account = new ClaveAccount(event.params.accountAddress);
 
-        account.implementation = Bytes.fromHexString(
-            '0xf5bEDd0304ee359844541262aC349a6016A50bc6',
-        );
         account.hasRecovery = false;
         account.isRecovering = false;
         account.recoveryCount = 0;
+        account.creationDate = ZERO;
     }
 
+    account.implementation = Bytes.fromHexString(
+        '0xf5bEDd0304ee359844541262aC349a6016A50bc6',
+    );
     account.deployDate = event.block.timestamp;
 
+    week.save();
     account.save();
 }
