f07a5d02a1a50bd7f7634b719fdacf53be13a9f6
tahos81 <tahirozpala@gmail.com>
2024-05-06 00:06:00 +0200

    feat: balances-subgraph factory mapper

--- balances-subgraph/src/account-factory.ts
+++ balances-subgraph/src/account-factory.ts
@@ -1,70 +1,31 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  AccountFactory,
-  ClaveAccountCreated,
-  ClaveAccountDeployed,
-  DeployerChanged,
-  ImplementationChanged,
-  OwnershipTransferred,
-  RegistryChanged
-} from "../generated/AccountFactory/AccountFactory"
-import { ExampleEntity } from "../generated/schema"
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 
-export function handleClaveAccountCreated(event: ClaveAccountCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+import { Bytes, ethereum, json } from '@graphprotocol/graph-ts';
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { ClaveAccountCreated as ClaveAccountCreatedEvent } from '../generated/AccountFactory/AccountFactory';
+import { ClaveAccount } from '../generated/schema';
+import { wallets } from '../wallets';
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function handleOnce(_block: ethereum.Block): void {
+    const walletsJson = json.fromString(wallets).toArray();
+    walletsJson.forEach((element) => {
+        const accountAddress = element.toObject().entries[0].value.toString();
+        const account = new ClaveAccount(Bytes.fromHexString(accountAddress));
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.accountAddress = event.params.accountAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.deployAccount(...)
-  // - contract.getAddressForSalt(...)
-  // - contract.getAddressForSaltAndImplementation(...)
-  // - contract.implementation(...)
-  // - contract.owner(...)
-  // - contract.proxyBytecodeHash(...)
-  // - contract.registry(...)
+        account.save();
+    });
 }
 
-export function handleClaveAccountDeployed(event: ClaveAccountDeployed): void {}
-
-export function handleDeployerChanged(event: DeployerChanged): void {}
-
-export function handleImplementationChanged(
-  event: ImplementationChanged
-): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleRegistryChanged(event: RegistryChanged): void {}
+export function handleClaveAccountCreated(
+    event: ClaveAccountCreatedEvent,
+): void {
+    const account = new ClaveAccount(event.params.accountAddress);
+    account.save();
+}
