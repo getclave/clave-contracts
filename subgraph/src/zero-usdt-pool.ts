8efa7412149a75de03f71a8511e331d727ce26b4
tahos81 <tahirozpala@gmail.com>
2024-06-12 16:25:33 +0300

    zerolend mapper

--- subgraph/src/zero-usdt-pool.ts
+++ subgraph/src/zero-usdt-pool.ts
@@ -1,38 +1,64 @@
-import { Supply, Withdraw } from '../generated/schema';
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import { ClaveAccount } from '../generated/schema';
 import {
     Supply as SupplyEvent,
     Withdraw as WithdrawEvent,
 } from '../generated/zeroUsdtPool/zeroUsdtPool';
+import {
+    getOrCreateDay,
+    getOrCreateMonth,
+    getOrCreateWeek,
+    getTotal,
+} from './helpers';
 
 export function handleSupply(event: SupplyEvent): void {
-    let entity = new Supply(
-        event.transaction.hash.concatI32(event.logIndex.toI32()),
-    );
-    entity.reserve = event.params.reserve;
-    entity.user = event.params.user;
-    entity.onBehalfOf = event.params.onBehalfOf;
-    entity.amount = event.params.amount;
-    entity.referralCode = event.params.referralCode;
+    const account = ClaveAccount.load(event.params.onBehalfOf);
+    if (!account) {
+        return;
+    }
 
-    entity.blockNumber = event.block.number;
-    entity.blockTimestamp = event.block.timestamp;
-    entity.transactionHash = event.transaction.hash;
+    const amount = event.params.amount;
 
-    entity.save();
+    const day = getOrCreateDay(event.block.timestamp);
+    const week = getOrCreateWeek(event.block.timestamp);
+    const month = getOrCreateMonth(event.block.timestamp);
+    const total = getTotal();
+
+    day.investIn = day.investIn.plus(amount);
+    week.investIn = week.investIn.plus(amount);
+    month.investIn = month.investIn.plus(amount);
+    total.invested = total.invested.plus(amount);
+    account.invested = account.invested.plus(amount);
+
+    day.save();
+    week.save();
+    month.save();
+    total.save();
+    account.save();
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
-    let entity = new Withdraw(
-        event.transaction.hash.concatI32(event.logIndex.toI32()),
-    );
-    entity.reserve = event.params.reserve;
-    entity.user = event.params.user;
-    entity.to = event.params.to;
-    entity.amount = event.params.amount;
+    const account = ClaveAccount.load(event.params.to);
+    if (!account) {
+        return;
+    }
 
-    entity.blockNumber = event.block.number;
-    entity.blockTimestamp = event.block.timestamp;
-    entity.transactionHash = event.transaction.hash;
+    const amount = event.params.amount;
 
-    entity.save();
+    const day = getOrCreateDay(event.block.timestamp);
+    const week = getOrCreateWeek(event.block.timestamp);
+    const month = getOrCreateMonth(event.block.timestamp);
+    const total = getTotal();
+
+    day.investOut = day.investOut.plus(amount);
+    week.investOut = week.investOut.plus(amount);
+    month.investOut = month.investOut.plus(amount);
+    total.invested = total.invested.minus(amount);
+    account.invested = account.invested.minus(amount);
+
+    day.save();
+    week.save();
+    month.save();
+    total.save();
+    account.save();
 }
