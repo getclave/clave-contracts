9802819a6e26bf21399c48b58dd25de24d01b5c8
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2024-06-24 19:48:44 +0300

    [clave-contracts] feat: registry migration file

---
+++ deploy/migrate-registry.ts
@@ -0,0 +1,53 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+import { readFile } from 'fs/promises';
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { Wallet } from 'zksync-ethers';
+
+const CLAVE_NAME_SERVICE_ADDRESS = '0x';
+const CHUNK_SIZE = 10;
+const STARTING_INDEX = 0;
+
+export default async function (hre: HardhatRuntimeEnvironment): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const privateKey = process.env.PRIVATE_KEY!;
+    const wallet = new Wallet(privateKey);
+
+    const registry = await hre.ethers.getContractAt(
+        'ClaveNameService',
+        CLAVE_NAME_SERVICE_ADDRESS,
+        wallet,
+    );
+
+    let currentIndex = STARTING_INDEX;
+    try {
+        const fileContent = await readFile(
+            './deploy/migration-data.json',
+            'utf-8',
+        );
+        const data: { result: { rows: Array<{ address: string }> } } =
+            JSON.parse(fileContent);
+
+        const addresses = data.result.rows.map((row) => row.address);
+
+        while (currentIndex < addresses.length) {
+            const chunk = addresses.slice(
+                currentIndex,
+                currentIndex + CHUNK_SIZE,
+            );
+
+            const tx = await registry.connect(wallet).registerMultiple(chunk);
+            await tx.wait();
+
+            console.log('chunk processed: ', currentIndex);
+
+            currentIndex += CHUNK_SIZE;
+        }
+    } catch (e) {
+        console.log('Error while processing chunk:', currentIndex);
+        console.log(e);
+    }
+}
