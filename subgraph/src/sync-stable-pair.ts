b958606773244e3a99073c8686fa8955b735a3f3
tahos81 <tahirozpala@gmail.com>
2024-07-22 08:11:18 +0300

    fix sync pool mapping file

--- subgraph/src/sync-stable-pair.ts
+++ subgraph/src/sync-stable-pair.ts
@@ -27,6 +27,9 @@ const protocol = 'SyncSwap';
 const token = Address.fromHexString(
     '0x000000000000000000000000000000000000800A',
 );
+const tokenb = Address.fromHexString(
+    '0x703b52F2b28fEbcB60E1372858AF5b18849FE867',
+);
 
 export function handleMint(event: MintEvent): void {
     const account = ClaveAccount.load(event.transaction.from);
@@ -37,10 +40,6 @@ export function handleMint(event: MintEvent): void {
     const pool = event.address;
     const amount0 = event.params.amount0;
     const amount1 = event.params.amount1;
-
-    if (amount1.gt(ZERO)) {
-        return;
-    }
 
     const day = getOrCreateDay(event.block.timestamp);
     const week = getOrCreateWeek(event.block.timestamp);
@@ -61,10 +60,34 @@ export function handleMint(event: MintEvent): void {
     monthlyEarnFlow.amountIn = monthlyEarnFlow.amountIn.plus(amount0);
     earnPosition.invested = earnPosition.invested.plus(amount0);
 
+    const dailyEarnFlow2 = getOrCreateDailyEarnFlow(day, tokenb, protocol);
+    const weeklyEarnFlow2 = getOrCreateWeeklyEarnFlow(week, tokenb, protocol);
+    const monthlyEarnFlow2 = getOrCreateMonthlyEarnFlow(
+        month,
+        tokenb,
+        protocol,
+    );
+    const earnPosition2 = getOrCreateEarnPosition(
+        account,
+        pool,
+        tokenb,
+        protocol,
+    );
+
+    dailyEarnFlow2.amountIn = dailyEarnFlow2.amountIn.plus(amount1);
+    weeklyEarnFlow2.amountIn = weeklyEarnFlow2.amountIn.plus(amount1);
+    monthlyEarnFlow2.amountIn = monthlyEarnFlow2.amountIn.plus(amount1);
+    earnPosition2.invested = earnPosition2.invested.plus(amount1);
+
     dailyEarnFlow.save();
     weeklyEarnFlow.save();
     monthlyEarnFlow.save();
     earnPosition.save();
+
+    dailyEarnFlow2.save();
+    weeklyEarnFlow2.save();
+    monthlyEarnFlow2.save();
+    earnPosition2.save();
 }
 
 export function handleBurn(event: BurnEvent): void {
@@ -76,10 +99,6 @@ export function handleBurn(event: BurnEvent): void {
     const pool = event.address;
     const amount0 = event.params.amount0;
     const amount1 = event.params.amount1;
-
-    if (amount1.gt(ZERO)) {
-        return;
-    }
 
     const day = getOrCreateDay(event.block.timestamp);
     const week = getOrCreateWeek(event.block.timestamp);
@@ -118,8 +137,50 @@ export function handleBurn(event: BurnEvent): void {
         earnPosition.invested = earnPosition.invested.minus(amount0);
     }
 
+    const dailyEarnFlow2 = getOrCreateDailyEarnFlow(day, tokenb, protocol);
+    const weeklyEarnFlow2 = getOrCreateWeeklyEarnFlow(week, tokenb, protocol);
+    const monthlyEarnFlow2 = getOrCreateMonthlyEarnFlow(
+        month,
+        tokenb,
+        protocol,
+    );
+    const earnPosition2 = getOrCreateEarnPosition(
+        account,
+        pool,
+        tokenb,
+        protocol,
+    );
+
+    const invested2 = earnPosition2.invested;
+
+    if (amount1.gt(invested2)) {
+        const compoundGain2 = amount1.minus(invested2);
+        dailyEarnFlow2.amountOut = dailyEarnFlow2.amountOut.plus(invested2);
+        dailyEarnFlow2.claimedGain =
+            dailyEarnFlow2.claimedGain.plus(compoundGain2);
+        weeklyEarnFlow2.amountOut = weeklyEarnFlow2.amountOut.plus(invested2);
+        weeklyEarnFlow2.claimedGain =
+            weeklyEarnFlow2.claimedGain.plus(compoundGain2);
+        monthlyEarnFlow2.amountOut = monthlyEarnFlow2.amountOut.plus(invested2);
+        monthlyEarnFlow2.claimedGain =
+            monthlyEarnFlow2.claimedGain.plus(compoundGain2);
+        earnPosition2.invested = ZERO;
+        earnPosition2.compoundGain =
+            earnPosition2.compoundGain.plus(compoundGain2);
+    } else {
+        dailyEarnFlow2.amountOut = dailyEarnFlow2.amountOut.plus(amount1);
+        weeklyEarnFlow2.amountOut = weeklyEarnFlow2.amountOut.plus(amount1);
+        monthlyEarnFlow2.amountOut = monthlyEarnFlow2.amountOut.plus(amount1);
+        earnPosition2.invested = earnPosition2.invested.minus(amount1);
+    }
+
     dailyEarnFlow.save();
     weeklyEarnFlow.save();
     monthlyEarnFlow.save();
     earnPosition.save();
+
+    dailyEarnFlow2.save();
+    weeklyEarnFlow2.save();
+    monthlyEarnFlow2.save();
+    earnPosition2.save();
 }
