3c2cea6f09be72c0730f1ac802747f53330b39ab
tahos81 <tahirozpala@gmail.com>
2024-08-01 16:17:11 +0300

    feat: generic deploy script

---
+++ deploy/deploy.ts
@@ -0,0 +1,12 @@
+import * as hre from 'hardhat';
+
+import { deployContract } from './utils';
+
+// An example of a basic deploy script
+// Do not push modifications to this file
+// Just modify, interact then revert changes
+export default async function (): Promise<void> {
+    const contractArtifactName = 'Example';
+    const constructorArguments = ['Clave', 324];
+    await deployContract(hre, contractArtifactName, constructorArguments);
+}
