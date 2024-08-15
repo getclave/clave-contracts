71a45d7987720c2af26cb1c07ae86339a7a0f465
tahos81 <tahirozpala@gmail.com>
2024-08-15 10:54:00 +0300

    husky

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 import '@matterlabs/hardhat-zksync';
 import '@nomicfoundation/hardhat-chai-matchers';
 import '@nomicfoundation/hardhat-ethers';
