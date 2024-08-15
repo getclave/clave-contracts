1903c88cc5cc094dacc980b1a7d9bd8de7fcbab4
tahos81 <tahirozpala@gmail.com>
2024-08-15 13:29:26 +0300

    feat: update contract

--- contracts/swap/SwapReferralFeePayer.sol
+++ contracts/swap/SwapReferralFeePayer.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.17;
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
-contract ReferralFeePayer {
+contract SwapReferralFeePayer {
     using SafeERC20 for IERC20;
 
-    event ReferralFee(address indexed referrer, address indexed token, uint256 fee);
+    event ReferralFee(address indexed receiver, address indexed token, uint256 fee);
 
-    function payFee(address referrer, address token, uint256 fee) external payable {
+    function payFee(
+        address referrer,
+        address token,
+        uint256 feeReferrer,
+        uint256 feeReferred
+    ) external payable {
         if (token == address(0)) {
             (bool success, ) = payable(referrer).call{value: fee}('');
             require(success, 'ReferralFeePayer: failed to pay referral fee');
         } else {
             IERC20 erc20 = IERC20(token);
-            erc20.safeTransferFrom(msg.sender, referrer, fee);
+            erc20.safeTransferFrom(msg.sender, referrer, feeReferrer);
         }
 
-        emit ReferralFee(referrer, token, fee);
+        emit ReferralFee(referrer, token, feeReferrer);
+        emit ReferralFee(msg.sender, token, feeReferred);
     }
 }
