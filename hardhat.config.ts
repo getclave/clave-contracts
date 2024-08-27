b99cd81b9d0c7f89779e906df8690d24500546a6
asgarovf <fasgarov2002@gmail.com>
2024-08-28 00:57:52 +0300

    [clave-contracts] fix: mvp deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -57,10 +57,10 @@ const config: HardhatUserConfig = {
                   }
                 : undefined,
             libraries: {
-                  "contracts/libraries/StringUtils.sol": {
-                    "StringUtils": "0x7e390c46302Fb6D7f6C7b4e36937287eB678FBC0"
-                  }
-                }
+                'contracts/libraries/StringUtils.sol': {
+                    StringUtils: '0x7e390c46302Fb6D7f6C7b4e36937287eB678FBC0',
+                },
+            },
         },
     },
     defaultNetwork: 'zkSyncSepolia',
