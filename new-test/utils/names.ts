af6ef7bcdefc73086cc452c50576d26ad5e0a3de
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2024-09-10 21:54:38 +0300

    [clave-contracts] feat: deployer test class

---
+++ new-test/utils/names.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+export const CONTRACT_NAMES = {
+    BATCH_CALLER: 'BatchCaller',
+    REGISTRY: 'ClaveRegistry',
+    IMPLEMENTATION: 'ClaveImplementation',
+    PROXY: 'ClaveProxy',
+    FACTORY: 'AccountFactory',
+    MOCK_VALIDATOR: 'MockValidator',
+};
+
+export enum VALIDATORS {
+    MOCK = 'MockValidator',
+    TEE = 'TeeValidator',
+    EOA = 'EOAValidator',
+    PASSKEY = 'PasskeyValidator',
+}
