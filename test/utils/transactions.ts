3e8d28c45731fffd298c5b5768ee425f13d00f0b
tahos81 <tahirozpala@gmail.com>
2024-09-20 15:13:12 +0300

    Update transactions.ts

--- test/utils/transactions.ts
+++ test/utils/transactions.ts
@@ -11,6 +11,7 @@ import { EIP712Signer, utils } from 'zksync-ethers';
 
 import type { CallStruct } from '../../typechain-types/contracts/batch/BatchCaller';
 import { sign } from './p256';
+import { getSignedData } from './passkey';
 
 export const ethTransfer = (
     to: string,
@@ -269,5 +270,39 @@ export async function preparePasskeyTx(
     hookData: Array<ethers.BytesLike> = [],
     paymasterParams?: types.PaymasterParams,
 ): Promise<types.TransactionLike> {
-    throw new Error('Not implemented');
+    if (tx.value == undefined) {
+        tx.value = parseEther('0');
+    }
+
+    tx = {
+        ...tx,
+        from: await account.getAddress(),
+        nonce: await provider.getTransactionCount(await account.getAddress()),
+        gasLimit: 30_000_000,
+        gasPrice: await provider.getGasPrice(),
+        chainId: (await provider.getNetwork()).chainId,
+        type: 113,
+        customData: {
+            gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
+            paymasterParams: paymasterParams,
+        } as types.Eip712Meta,
+    };
+
+    const signedTxHash = EIP712Signer.getSignedDigest(tx);
+
+    const abiCoder = ethers.AbiCoder.defaultAbiCoder();
+    let signature = sign(getSignedData(signedTxHash.toString()), keyPair);
+    signature = '0x01' + signature.slice(2);
+
+    signature = abiCoder.encode(
+        ['bytes', 'address', 'bytes[]'],
+        [signature, validatorAddress, hookData],
+    );
+
+    tx.customData = {
+        ...tx.customData,
+        customSignature: signature,
+    };
+
+    return tx;
 }
